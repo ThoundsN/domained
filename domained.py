90641d961b16d35cc5c7a124a6472c27e3cb28fe
ThoundsN <inthebybyby@gmail.com>
2019-05-03 00:16:38 +0800

    s

--- domained.py
+++ domained.py
@@ -111,7 +111,7 @@ def banner():
 
 
 def runProcess(exe):
-    p = subprocess.Popen(exe, stdout=subprocess.PIPE, stderr=subprocess.STDOUT,shell=True)
+    p = subprocess.Popen(exe, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     while(True):
         # returns None while subprocess is running
         retcode = p.poll()
